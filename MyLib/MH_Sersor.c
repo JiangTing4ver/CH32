4b1a073efac4feadf02dcd41029c811a116f288f
JiangTing4ver <toquangsbcocat7280@gmail.com>
2025-12-20 23:16:22 +0800

    docs(MH_Sersor): 添加传感器模块的函数注释
    添加了MH_Sersor_Init和Note函数的详细注释，说明其功能和参数

--- MyLib/MH_Sersor.c
+++ MyLib/MH_Sersor.c
@@ -1,9 +1,24 @@
 #include "MH_Sersor.h"
-
+/**
+ * @file MH_Sersor.c
+ * @author 4verJiangting
+ * @brief 获取红外模块的状态
+ * @version 0.1
+ * @date 2025-12-20
+ */
+/**
+ * @brief 初始化传感器
+ * @param RCC_Periph_MH_Sersor 传感器端口时钟
+ * @param MH_Sersor_Port 传感器端口
+ * @param MH_Sersor_Pin 传感器引脚
+ */
 void MH_Sersor_Init()
 {
     GPIO_IN_SetInit(RCC_Periph_MH_Sersor, MH_Sersor_Port, MH_Sersor_Pin);
 }
+/**
+ * @brief 提示传感器状态
+ */
 void Note()
 {
     LED_Init();
