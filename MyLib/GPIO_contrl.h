f00bd193398af7710f866d93404eab02f481a9cd
JiangTing4ver <toquangsbcocat7280@gmail.com>
2025-12-20 23:14:22 +0800

    feat(GPIO控制): 添加GPIO输入初始化功能
    新增GPIO_IN_SetInit函数用于配置GPIO为浮空输入模式，完善GPIO控制库功能

--- MyLib/GPIO_contrl.h
+++ MyLib/GPIO_contrl.h
@@ -9,4 +9,5 @@
 
 // 函数声明
 void GPIO_OUT_SetInit(uint32_t RCC_Periph_GPIOx, GPIO_TypeDef* GPIO_Port, uint16_t GPIO_Pin);
+void GPIO_IN_SetInit(uint32_t RCC_Periph_GPIOx, GPIO_TypeDef* GPIO_Port, uint16_t GPIO_Pin);
 #endif
