304502ce142697347e8f6311111f225a24bb0467
JiangTing4ver <toquangsbcocat7280@gmail.com>
2025-12-20 23:16:30 +0800

    feat: 添加功能选择标志支持LED和传感器功能
    添加条件编译标志flag，支持在LED功能和传感器功能之间切换

--- Application/main.c
+++ Application/main.c
@@ -1,7 +1,14 @@
 #include "ch32f10x.h"
 #include "GPIO_contrl.h"
 #include "LED.h"
+#include "MH_Sersor.h"
 
+#define LED 0
+#define HM 1
+
+// 在这里定义flag，选择使用哪个功能
+// #define flag LED  // 使用LED功能
+#define flag HM     // 使用传感器功能
 
 
 int main(void)
@@ -12,12 +19,21 @@ int main(void)
     
     // 初始化延时功能
     Delay_Init();
-    
-    // 初始化LED
-    LED_Init();
+
+    #if flag == LED
+        // 初始化LED
+        LED_Init();
+    #elif flag == HM
+        // 初始化传感器
+        MH_Sersor_Init();
+    #endif
     
     while(1)
     {
-        LED_blink();
+        #if flag == LED
+            LED_blink();
+        #elif flag == HM
+            Note();
+        #endif  
     }
 }
